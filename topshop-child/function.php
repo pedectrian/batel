fe840c16bde644eb36197a421e4bc105d7e59cee
Alexander Permyakov <pedectrian@gmail.com>
2015-03-05 22:11:00 +0300

    Site logo action for jetpack

---
+++ topshop-child/function.php
@@ -0,0 +1,21 @@
+<?php
+if ( ! function_exists( 'batel_site_branding' ) ) {
+    /**
+     * Display Site Branding
+     * @since  1.0.0
+     * @return void
+     */
+    function batel_site_branding() {
+        if ( function_exists( 'jetpack_has_site_logo' ) && jetpack_has_site_logo() ) {
+            jetpack_the_site_logo();
+        } else { ?>
+            <div class="site-header-left">
+                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+            </div><!-- .site-branding -->
+        <?php }
+    }
+}
+
+add_action( 'batel_header', 'batel_site_branding', 20 );
+?>
