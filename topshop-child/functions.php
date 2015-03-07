813d41077474d8c2e0e66bf85c5473c6a33b5812
Alexander Permyakov <pedectrian@gmail.com>
2015-03-07 12:27:17 +0300

    remove customizer options

--- topshop-child/functions.php
+++ topshop-child/functions.php
@@ -68,15 +68,15 @@ function removeCustomizerOptions(){
     $customizer_library = Customizer_Library::Instance();
 
     foreach ($customizer_library->get_options() as $key => $options) {
-        print_r($options);
 
-        if (isset($options[0])) {
+        if (isset($options[0]) && is_array($options[0])) {
             foreach ($options as $sectionKey => $section) {
                 if (isset($section['id']) && $section['id'] == 'topshop-social') {
                     unset($options[$sectionKey]);
                 }
             }
         }
+        print_r($options);
     }
 
     die();
