488a49866de8ea159c9ebeffc7d1a5e2d571cc2a
Alexander Permyakov <pedectrian@gmail.com>
2015-03-07 12:03:05 +0300

    remove customizer options

--- topshop-child/functions.php
+++ topshop-child/functions.php
@@ -70,7 +70,7 @@ $batel = new Batel();
 
 
 add_action( 'init', array( $batel, 'addCustomizerOptions' ) );
-add_action( 'customize_register', array( $batel, 'removeCustomizerOptions' ),  40 );
+add_action( 'init', array( $batel, 'removeCustomizerOptions' ),  40 );
 add_action( 'customizer_library_styles', array( $batel, 'customizerBuildStyles' ) );
 
 add_action( 'after_setup_theme', 'batel_setup' );
