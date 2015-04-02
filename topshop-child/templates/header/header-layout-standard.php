109cc16849d64502087e749360529e8685f485ca
Alexander Permyakov <pedectrian@gmail.com>
2015-04-02 20:53:31 +0300

    sidebar

--- topshop-child/templates/header/header-layout-standard.php
+++ topshop-child/templates/header/header-layout-standard.php
@@ -74,6 +74,7 @@
     <div class="clearboth"></div>
     
 </div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script>
     $(document).ready(function(){
         $('.product-categories > .cat-item').append('<div class="open"></div>');
