23ea5aaa2519fbfb001a204079c7e4709554ed62
Alexander Permyakov <pedectrian@gmail.com>
2015-04-08 22:43:26 +0300

    top menu styles

--- topshop-child/functions.php
+++ topshop-child/functions.php
@@ -9,7 +9,7 @@ if ( ! function_exists( 'batel_site_branding' ) ) {
     function batel_site_branding() {
         if ( function_exists( 'jetpack_has_site_logo' ) && jetpack_has_site_logo() ) { ?>
             <a href="http://batel-store.ru/" class="site-logo-link" rel="home" itemprop="url">
-                <img width="199" height="131" src="http://batel-store.ru/wp-content/uploads/2015/04/white-logo-v21.png" class="site-logo attachment-full" alt="white-logo-v2" data-size="full" itemprop="logo">
+                <img width="199" height="131" src="http://batel-store.ru/wp-content/uploads/2015/03/logo2.png" class="site-logo attachment-full" alt="white-logo-v2" data-size="full" itemprop="logo">
             </a>
        <?php } else { ?>
             <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
