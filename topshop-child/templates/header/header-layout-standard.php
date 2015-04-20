12fa26952f020f2331c746b8ebdcfc2f17feb3ab
Alexander Permyakov <pedectrian@gmail.com>
2015-04-20 21:47:53 +0300

    js bug fix

--- topshop-child/templates/header/header-layout-standard.php
+++ topshop-child/templates/header/header-layout-standard.php
@@ -72,7 +72,7 @@
         
     </div>
     <div class="clearboth"></div>
-    <div class="header-stick" style="display: none;"></div>
+
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script>
