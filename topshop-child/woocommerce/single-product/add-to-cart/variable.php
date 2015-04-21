445a3fb958817aa50a2f6b7d17bde3e2a8ef3d92
Alexander Permyakov <pedectrian@gmail.com>
2015-04-22 00:18:20 +0300

    cart-fix

--- topshop-child/woocommerce/single-product/add-to-cart/variable.php
+++ topshop-child/woocommerce/single-product/add-to-cart/variable.php
@@ -60,8 +60,8 @@ global $product, $post;
 						</select>
 							<?php
 								foreach ( $available_variations as $option ) {
-									$opt = $options['attributes'][0]['attributes_pa_shades'];
-									var_dump($options['attributes'][0]);
+									$opt = $options['attributes']['attributes_pa_shades'];
+									var_dump($options['attributes']);
 									echo '<span class="variation-img" data-variation="' . esc_attr( sanitize_title( $opt ) ) . '"><img title="' . $opt . '" src="' . $option['image_src'] . '"/></span>';
 								}
 							?>
